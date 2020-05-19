5395d56d14fbf53566ea47772509dbb7456097f0
sweetxxin <42604212+sweetxxin@users.noreply.github.com>
2020-05-19 12:17:06 +0800

    Update MailUtil.java

--- src/main/java/com/xxin/punch/MailUtil.java
+++ src/main/java/com/xxin/punch/MailUtil.java
@@ -8,9 +8,9 @@ import org.apache.commons.mail.HtmlEmail;
 import java.util.Date;
 
 public class MailUtil {
-     static String from="jessic_xxin@aliyun.com";
-     static String to="1208668915@qq.com";
-     static String auth="chen18926278337";
+     static String from="xxxxx@aliyun.com";
+     static String to="xxxxx@qq.com";
+     static String auth="xxxxxxx";
      static String subject="打卡通知";
      static String content="打卡成功";
      static String host="smtp.aliyun.com";
